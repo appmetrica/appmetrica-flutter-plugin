f4999617604304faded5210f80eddb00aa720144
kondratyonok <kondratyonok@yandex-team.com>
2026-01-08 12:48:44 +0300

    add pigeon check to pr actions commit_hash:2670e640d941edffa4fb15bc76d10f82a48beef6

--- generate_pigeon.sh
+++ generate_pigeon.sh
@@ -3,4 +3,4 @@
 set -e
 
 mkdir -p android/src/main/java/io/appmetrica/analytics/flutter/pigeon
-dart run pigeon --input pigeons/appmetrica_api.dart
+"${1:-dart}" run pigeon --input pigeons/appmetrica_api.dart
